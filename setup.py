eecc2b9b0bb77f2caa9a803b4edf729241130b55
orionpax00 <durgesh123.iitr@gmail.com>
2020-01-16 19:18:12 +0530

    Base structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="tfproject",
+    version="1.0.0",
+    author="Durgesh",
+    description="description",
+    license="APACHE",
+    zip_safe=True,
+)
---
+++ tfproject/misc/config.py
@@ -0,0 +1,16 @@
+from yacs.config import CfgNode as CN
+
+
+# Initial configurations
+_C = CN()
+
+_C.ENVIRONMENT = CN()
+_C.ENVIRONMENT.NAME = "tfproject" 
+_C.ENVIRONMENT.TRAIL_NUMBER = 0 #operating system specific 
+
+_C.OPTIMIZER = CN() #training configurations
+_C.OPTIMIZER.LR = 0.0001 #learning rate
+
+
+def get_cfg_defaults():
+    return _C.clone()
