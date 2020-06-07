da8b8ea7bc1cf345ed4095c30c11e1b1cb64683d
orionpax00 <durgesh123.iitr@gmail.com>
2020-06-07 22:41:52 +0530

    tensorboard config

--- yapl/setup/googleColab.py
+++ yapl/setup/googleColab.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+import time
 
 def changeDir(folder_name):
     os.chdir("/content/{}".format(folder_name))
@@ -14,9 +16,34 @@ def makerunFile(folder_name):
         runfile.write("sh ./train.sh")
 
 def setupColab(githubLink):
-    # os.system("git clone {}".format(githubLink))
+    """
+        example:
+            setupColab("https://github.com/orionpax00/yapl.git")
+            !./runcolab.sh
+
+    """
+    os.system("git clone {}".format(githubLink))
 
     folder_name = githubLink.split("/")[-1].split('.')[0]
     makerunFile(folder_name)
     changeDir(folder_name)
     changePer(folder_name)
+
+def setupTensorboard():
+    os.system("wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip")
+    os.system("unzip ngrok-stable-linux-amd64.zip")
+    print("Starting ngrok")
+    os.system("./ngrok http 6006 &")
+    print("Starting tensorboard")
+    os.system("tensorboard --logdir log --host 0.0.0.0 --port 6006 &")
+    
+    time.sleep(5)
+
+    
+    output = subprocess.check_output("curl -s http://localhost:4040/api/tunnels", shell=True)
+    url_ = json.loads(output)['tunnels'][0]['public_url']
+    
+    print("---------------------\nIF:")
+    print("Make sure that a web service is running on localhost:6006 and that it is a valid address.")
+    print("TRY TO RUN TENSORBOARD MANUALLY")
+    return url_
