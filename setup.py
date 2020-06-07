3cec30fb253a7f22b1cac2ca5189192c31130596
Durgesh <durgesh123.iitr@gmail.com>
2020-06-07 11:12:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
 setup(
-    name="tfproject",
-    version="1.0.0",
+    name="yapl",
+    version="0.0.1",
     author="Durgesh",
-    description="description",
+    author_email="dkumar@ce.iitr.ac.in",
+    description="yet another python library",
+    url="https://github.com/orionpax00/yapl",
+    packages=setuptools.find_packages(),
     license="APACHE",
-    zip_safe=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
