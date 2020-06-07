c2d3f563296f8f5cab46d839d3560ba2d9954533
Durgesh <durgesh123.iitr@gmail.com>
2020-06-07 08:53:58 +0000

    new version published

---
+++ yapl/data/loader/imageloader.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import torch
+
+from PIL import Image
+
+class SimpleLoader:
+
+    def __init__(self, images, targets):
+        self.images = images
+        self.targets = targets
+
+    def __getitem__(self, item):
+        img = Image.open(self.images[item])
+        target = self.targets[item]
+
+        return {
+            "image" : torch.tensor(img, dtype=torch.float),
+            'target' : torch.tensor(target, dtype=torch.long)
+        }
